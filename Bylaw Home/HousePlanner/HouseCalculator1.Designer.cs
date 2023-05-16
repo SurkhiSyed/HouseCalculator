8ff9be992f3f251b04127f81e3ca5f4a422caf54
Syed Kaab Surkhi <86132908+SurkhiSyed@users.noreply.github.com>
2023-05-16 09:39:44 -0400

    Add files via upload

---
+++ Bylaw Home/HousePlanner/HouseCalculator1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace HousePlanner
+{
+    partial class HouseCalculator1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LengthLot_Label = new System.Windows.Forms.Label();
+            this.LengthLot_txt = new System.Windows.Forms.TextBox();
+            this.WidthLot_txt = new System.Windows.Forms.TextBox();
+            this.WidthLot_Label = new System.Windows.Forms.Label();
+            this.HouseLength_Label = new System.Windows.Forms.Label();
+            this.HouseLength_txt = new System.Windows.Forms.TextBox();
+            this.HouseWidth_Label = new System.Windows.Forms.Label();
+            this.HouseWidth_txt = new System.Windows.Forms.TextBox();
+            this.Verify_btn = new System.Windows.Forms.Button();
+            this.OutputBox_txt = new System.Windows.Forms.ListBox();
+            this.NewCalc_btn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.FaceBox_img = new System.Windows.Forms.PictureBox();
+            this.ProjectLabel_label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FaceBox_img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LengthLot_Label
+            // 
+            this.LengthLot_Label.AutoSize = true;
+            this.LengthLot_Label.BackColor = System.Drawing.Color.Black;
+            this.LengthLot_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LengthLot_Label.ForeColor = System.Drawing.Color.White;
+            this.LengthLot_Label.Location = new System.Drawing.Point(1065, 11);
+            this.LengthLot_Label.Name = "LengthLot_Label";
+            this.LengthLot_Label.Size = new System.Drawing.Size(214, 29);
+            this.LengthLot_Label.TabIndex = 5;
+            this.LengthLot_Label.Text = "Length of the Lot:";
+            // 
+            // LengthLot_txt
+            // 
+            this.LengthLot_txt.Location = new System.Drawing.Point(1070, 42);
+            this.LengthLot_txt.Name = "LengthLot_txt";
+            this.LengthLot_txt.Size = new System.Drawing.Size(202, 26);
+            this.LengthLot_txt.TabIndex = 6;
+            // 
+            // WidthLot_txt
+            // 
+            this.WidthLot_txt.Location = new System.Drawing.Point(1070, 121);
+            this.WidthLot_txt.Name = "WidthLot_txt";
+            this.WidthLot_txt.Size = new System.Drawing.Size(202, 26);
+            this.WidthLot_txt.TabIndex = 7;
+            // 
+            // WidthLot_Label
+            // 
+            this.WidthLot_Label.AutoSize = true;
+            this.WidthLot_Label.BackColor = System.Drawing.Color.Black;
+            this.WidthLot_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WidthLot_Label.ForeColor = System.Drawing.Color.White;
+            this.WidthLot_Label.Location = new System.Drawing.Point(1065, 78);
+            this.WidthLot_Label.Name = "WidthLot_Label";
+            this.WidthLot_Label.Size = new System.Drawing.Size(201, 29);
+            this.WidthLot_Label.TabIndex = 8;
+            this.WidthLot_Label.Text = "Width of the Lot:";
+            // 
+            // HouseLength_Label
+            // 
+            this.HouseLength_Label.AutoSize = true;
+            this.HouseLength_Label.BackColor = System.Drawing.Color.Black;
+            this.HouseLength_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HouseLength_Label.ForeColor = System.Drawing.Color.White;
+            this.HouseLength_Label.Location = new System.Drawing.Point(1062, 299);
+            this.HouseLength_Label.Name = "HouseLength_Label";
+            this.HouseLength_Label.Size = new System.Drawing.Size(181, 29);
+            this.HouseLength_Label.TabIndex = 9;
+            this.HouseLength_Label.Text = "House Length:";
+            // 
+            // HouseLength_txt
+            // 
+            this.HouseLength_txt.Location = new System.Drawing.Point(1067, 334);
+            this.HouseLength_txt.Name = "HouseLength_txt";
+            this.HouseLength_txt.Size = new System.Drawing.Size(202, 26);
+            this.HouseLength_txt.TabIndex = 10;
+            // 
+            // HouseWidth_Label
+            // 
+            this.HouseWidth_Label.AutoSize = true;
+            this.HouseWidth_Label.BackColor = System.Drawing.Color.Black;
+            this.HouseWidth_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HouseWidth_Label.ForeColor = System.Drawing.Color.White;
+            this.HouseWidth_Label.Location = new System.Drawing.Point(1062, 374);
+            this.HouseWidth_Label.Name = "HouseWidth_Label";
+            this.HouseWidth_Label.Size = new System.Drawing.Size(168, 29);
+            this.HouseWidth_Label.TabIndex = 11;
+            this.HouseWidth_Label.Text = "House Width:";
+            // 
+            // HouseWidth_txt
+            // 
+            this.HouseWidth_txt.Location = new System.Drawing.Point(1067, 406);
+            this.HouseWidth_txt.Name = "HouseWidth_txt";
+            this.HouseWidth_txt.Size = new System.Drawing.Size(202, 26);
+            this.HouseWidth_txt.TabIndex = 12;
+            // 
+            // Verify_btn
+            // 
+            this.Verify_btn.BackColor = System.Drawing.Color.White;
+            this.Verify_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Verify_btn.Location = new System.Drawing.Point(1088, 176);
+            this.Verify_btn.Name = "Verify_btn";
+            this.Verify_btn.Size = new System.Drawing.Size(142, 38);
+            this.Verify_btn.TabIndex = 13;
+            this.Verify_btn.Text = "Verify";
+            this.Verify_btn.UseVisualStyleBackColor = false;
+            this.Verify_btn.Click += new System.EventHandler(this.Verify_btn_Click);
+            // 
+            // OutputBox_txt
+            // 
+            this.OutputBox_txt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OutputBox_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F);
+            this.OutputBox_txt.FormattingEnabled = true;
+            this.OutputBox_txt.ItemHeight = 17;
+            this.OutputBox_txt.Location = new System.Drawing.Point(840, 63);
+            this.OutputBox_txt.Name = "OutputBox_txt";
+            this.OutputBox_txt.Size = new System.Drawing.Size(216, 68);
+            this.OutputBox_txt.TabIndex = 15;
+            // 
+            // NewCalc_btn
+            // 
+            this.NewCalc_btn.BackColor = System.Drawing.Color.White;
+            this.NewCalc_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewCalc_btn.Location = new System.Drawing.Point(1088, 233);
+            this.NewCalc_btn.Name = "NewCalc_btn";
+            this.NewCalc_btn.Size = new System.Drawing.Size(142, 37);
+            this.NewCalc_btn.TabIndex = 18;
+            this.NewCalc_btn.Text = "New";
+            this.NewCalc_btn.UseVisualStyleBackColor = false;
+            this.NewCalc_btn.Click += new System.EventHandler(this.NewCalc_btn_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::HousePlanner.Properties.Resources.download__2_;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(1050, -4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 494);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::HousePlanner.Properties.Resources._141c7dd1c3b59bd4bec6cbd1d77d1806;
+            this.pictureBox3.Location = new System.Drawing.Point(828, -4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(236, 305);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            // 
+            // FaceBox_img
+            // 
+            this.FaceBox_img.Image = global::HousePlanner.Properties.Resources.happy4;
+            this.FaceBox_img.Location = new System.Drawing.Point(828, 300);
+            this.FaceBox_img.Name = "FaceBox_img";
+            this.FaceBox_img.Size = new System.Drawing.Size(228, 194);
+            this.FaceBox_img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FaceBox_img.TabIndex = 16;
+            this.FaceBox_img.TabStop = false;
+            // 
+            // ProjectLabel_label
+            // 
+            this.ProjectLabel_label.AutoSize = true;
+            this.ProjectLabel_label.BackColor = System.Drawing.Color.Transparent;
+            this.ProjectLabel_label.Font = new System.Drawing.Font("Gabriola", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProjectLabel_label.Location = new System.Drawing.Point(12, 439);
+            this.ProjectLabel_label.Name = "ProjectLabel_label";
+            this.ProjectLabel_label.Size = new System.Drawing.Size(219, 59);
+            this.ProjectLabel_label.TabIndex = 19;
+            this.ProjectLabel_label.Text = "Available Ground:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::HousePlanner.Properties.Resources.saul;
+            this.pictureBox1.Location = new System.Drawing.Point(960, 209);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // HouseCalculator1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BackgroundImage = global::HousePlanner.Properties.Resources.image_processing20210530_3563_11m0oye;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1289, 484);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ProjectLabel_label);
+            this.Controls.Add(this.NewCalc_btn);
+            this.Controls.Add(this.Verify_btn);
+            this.Controls.Add(this.HouseWidth_txt);
+            this.Controls.Add(this.HouseWidth_Label);
+            this.Controls.Add(this.HouseLength_txt);
+            this.Controls.Add(this.HouseLength_Label);
+            this.Controls.Add(this.WidthLot_Label);
+            this.Controls.Add(this.WidthLot_txt);
+            this.Controls.Add(this.LengthLot_txt);
+            this.Controls.Add(this.LengthLot_Label);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.OutputBox_txt);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.FaceBox_img);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "HouseCalculator1";
+            this.Text = "BylawHome";
+            this.Load += new System.EventHandler(this.HouseCalculator1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FaceBox_img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label LengthLot_Label;
+        private System.Windows.Forms.TextBox LengthLot_txt;
+        private System.Windows.Forms.TextBox WidthLot_txt;
+        private System.Windows.Forms.Label WidthLot_Label;
+        private System.Windows.Forms.Label HouseLength_Label;
+        private System.Windows.Forms.TextBox HouseLength_txt;
+        private System.Windows.Forms.Label HouseWidth_Label;
+        private System.Windows.Forms.TextBox HouseWidth_txt;
+        private System.Windows.Forms.Button Verify_btn;
+        private System.Windows.Forms.ListBox OutputBox_txt;
+        private System.Windows.Forms.PictureBox FaceBox_img;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button NewCalc_btn;
+        private System.Windows.Forms.Label ProjectLabel_label;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Bylaw Home/HousePlanner/HouseCalculator1.cs
@@ -0,0 +1,172 @@
+﻿//Syed Kaab Surkhi
+//April 14, 2023
+//Mr. A. MAyer
+//A House calculator that calculates possible house dimensions according to lot dimensions
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HousePlanner
+{
+    public partial class HouseCalculator1 : Form
+    {
+        public HouseCalculator1()
+        {
+            InitializeComponent();
+        }
+        //Create variables to store inputted dimensions
+        public string lotLength;
+        public string lotWidth;
+        public double houseLength;
+        public double houseWidth;
+        //Set the maximum pixels to draw for the lot width and height in the follwoing varibales
+        public int MaxWidthLot = 500;
+        public int MaxLengthLot = 250;
+        //Create a pen to draw the lot
+        Pen p = new Pen(Color.Blue, 3);
+        //Create variables to store the midpoint of the length and width of the lot
+        public double midpointX;
+        public double midpointY;
+        //Create a pen to draw the house
+        Pen w = new Pen(Color.Green, 3);
+        //Create a condition variable that stores wether imput values are good or bad
+        public string condition;
+
+        private void Verify_btn_Click(object sender, EventArgs e)
+        {
+            //Try the following code to make sure user has not imputted non integers
+            try
+            {
+                //If the inputted code is "00", then end the program
+                if (LengthLot_txt.Text == "00" || WidthLot_txt.Text == "00" || HouseLength_txt.Text == "00" || HouseWidth_txt.Text == "00")
+                {
+                    this.Close();
+                }
+                //Take user input and store it
+                lotLength = LengthLot_txt.Text;
+                lotWidth = WidthLot_txt.Text;
+                houseWidth = Convert.ToDouble(HouseWidth_txt.Text);
+                houseLength = Convert.ToDouble(HouseLength_txt.Text);
+                //Set start condition as "Right"
+                condition = "Right";
+                //Cear the items in itembox
+                OutputBox_txt.Items.Clear();
+
+                //Check if inputted values meet the requirements to build a house and set the condition as "wrong" if they dont match
+                //If there is less than 2 m (4 m total on vertical and 4 m on horizontal) of space between the lot and the house
+                if ((houseWidth > (Convert.ToDouble(lotWidth) - 4)) || (houseLength > (Convert.ToDouble(lotLength) - 4)))
+                {
+                    condition = "Wrong";
+                    OutputBox_txt.Items.Add("Clearance Requirements not met");
+                }
+                //IF house area is less than 25% of lot area
+                if ((houseLength * houseWidth) < 0.25 * ((Convert.ToDouble(lotWidth)) * (Convert.ToDouble(lotLength))))
+                {
+                    condition = "Wrong";
+                    OutputBox_txt.Items.Add("Does not Meet Min Area");
+                }
+                //If house area is greater than 40% of lot area
+                if ((houseLength * houseWidth) > 0.4 * ((Convert.ToDouble(lotWidth)) * (Convert.ToDouble(lotLength))))
+                {
+                    condition = "Wrong";
+                    OutputBox_txt.Items.Add("Max Area exceeded");
+                }
+                //If condition stays as "Right"
+                if (condition == "Right")
+                {
+                    OutputBox_txt.Items.Add("Fits the lot, congrats!");
+                    OutputBox_txt.Items.Add("Better call Saul!");
+                    FaceBox_img.Image = Properties.Resources.happy4;
+                    w.Color = Color.Green;
+                }
+                //IF condition is "Wrong"
+                if (condition == "Wrong")
+                {
+                    FaceBox_img.Image = Properties.Resources.sad2;
+                    OutputBox_txt.Items.Add("Let's change dimensions!");
+                    w.Color = Color.Red;
+
+                }
+
+                //Variables that will store the ratios of length to width for the lot
+                double Lengthratio = Convert.ToDouble(lotLength) / Convert.ToDouble(lotWidth);
+                double Widthratio = Convert.ToDouble(lotWidth) / Convert.ToDouble(lotLength);
+                //Variables that will store the rations of length to width of the house
+                double houseWidthRatio = Convert.ToDouble(houseWidth) / Convert.ToDouble(lotWidth);
+                double houseLengthRatio = Convert.ToDouble(houseLength) / Convert.ToDouble(lotLength);
+
+                //Drawing calculations
+                if (Lengthratio <= 0.5)
+                {
+                    int legnth = Convert.ToInt32(Math.Round(MaxWidthLot * Lengthratio, 0));
+                    Graphics g = this.CreateGraphics();
+                    Rectangle shape = new Rectangle(25, 25, MaxWidthLot, legnth);
+                    g.DrawRectangle(p, shape);
+
+                    midpointX = Math.Round((Convert.ToDouble(25 + 25 + MaxWidthLot) / 2), 0);
+                    midpointY = Math.Round((Convert.ToDouble(25 + 25 + legnth) / 2), 0);
+
+
+                    Graphics l = this.CreateGraphics();
+                    Rectangle shapes = new Rectangle(Convert.ToInt32(Math.Round((midpointX - ((houseWidthRatio * MaxWidthLot) / 2)), 0)), Convert.ToInt32(Math.Round((midpointY - ((houseLengthRatio * legnth) / 2)), 0)), Convert.ToInt32(houseWidthRatio * MaxWidthLot), Convert.ToInt32(houseLengthRatio * legnth));
+                    l.DrawRectangle(w, shapes);
+                }
+                else if (Widthratio <= 2)
+                {
+                    int width = Convert.ToInt32(Math.Round(MaxLengthLot * Widthratio, 0));
+                    Graphics g = this.CreateGraphics();
+                    Rectangle shape = new Rectangle(25, 25, width, MaxLengthLot);
+                    g.DrawRectangle(p, shape);
+
+                    midpointX = Math.Round((Convert.ToDouble(25 + 25 + width) / 2), 0);
+                    midpointY = Math.Round((Convert.ToDouble(25 + 25 + MaxLengthLot) / 2), 0);
+
+                    Graphics l = this.CreateGraphics();
+                    Rectangle shapes = new Rectangle(Convert.ToInt32(Math.Round((midpointX - ((houseWidthRatio * width) / 2)), 0)), Convert.ToInt32(Math.Round((midpointY - ((houseLengthRatio * MaxLengthLot) / 2)), 0)), Convert.ToInt32(houseWidthRatio * width), Convert.ToInt32(houseLengthRatio * MaxLengthLot));
+                    l.DrawRectangle(w, shapes);
+                }
+                else if (Lengthratio == 1)
+                {
+                    Graphics g = this.CreateGraphics();
+                    Rectangle shape = new Rectangle(25, 25, 250, 250);
+                    g.DrawRectangle(p, shape);
+                    midpointX = Math.Round((Convert.ToDouble(25 + 25 + 250) / 2), 0);
+
+                }
+                //Disbale the verify button
+                Verify_btn.Enabled = false;
+            }
+            //If inputted values are non-integers
+            catch
+            {
+                OutputBox_txt.Items.Clear();
+                OutputBox_txt.Items.Add("Please input proper dimensions!");
+            }
+        }
+        //If the "New" button is clicked
+        private void NewCalc_btn_Click(object sender, EventArgs e)
+        {
+            //Clear the drawing 
+            this.Invalidate();
+            OutputBox_txt.Items.Clear();
+            Verify_btn.Enabled = true;
+            FaceBox_img.Image = Properties.Resources.happy4;
+            OutputBox_txt.Items.Add("Welcome!");
+            OutputBox_txt.Items.Add("I'm here to help you!");
+        }
+
+        private void HouseCalculator1_Load(object sender, EventArgs e)
+        {
+            FaceBox_img.Image = Properties.Resources.happy4;
+            OutputBox_txt.Items.Add("Welcome!");
+            OutputBox_txt.Items.Add("I'm here to help you!");
+        }
+    }
+}
